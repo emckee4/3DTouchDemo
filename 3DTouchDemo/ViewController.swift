6d1b6797692c8e2ab5ef2624eaf8fec13fd837df
Evan McKee <emckee@gmail.com>
2015-09-26 13:41:09 -0700

    Initial Commit

---
+++ 3DTouchDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3DTouchDemo
+//
+//  Created by Evan Mckee on 9/26/15.
+//  Copyright © 2015 McKeeMaKer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
